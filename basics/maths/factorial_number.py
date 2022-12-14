045596454494acd084a5b74326d827a9194c2aa0
shafikshaon <shafikshaon@gmail.com>
2022-12-15 01:33:05 +0600

    Factorial Number Add

---
+++ basics/maths/factorial_number.py
@@ -0,0 +1,17 @@
+# Factorial number
+
+def main():
+    fact = 1
+
+    num = 5
+    if num == 0:
+        print(f"Factorial of {num} is: {fact}")
+    elif num < 0:
+        print(f"Factorial is not available for negative number.")
+    else:
+        for i in range(1, num + 1):
+            fact *= i
+        print(f"Factorial of {num} is: {fact}")
+
+
+main()
