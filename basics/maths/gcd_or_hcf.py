4f710ee7d10d2b7cce6caad3fe08b7879800ac93
shafikshaon <shafikshaon@gmail.com>
2022-12-15 02:39:46 +0600

    GCD or HCF Add

---
+++ basics/maths/gcd_or_hcf.py
@@ -0,0 +1,19 @@
+# GCD or HCF
+# HCF (Highest Common Factor) is also known as Greatest Common Divisor (GCD)
+
+
+def main():
+    first = 12
+    second = 18
+
+    gcd = 1
+    for i in range(1, first + 1):
+        if first % i == 0 and second % i == 0:
+            gcd = i
+    print('HCF or GCD of %d and %d is %d' % (first, second, gcd))
+
+    lcm = first * second / gcd
+    print('LCM of %d and %d is %d' % (first, second, lcm))
+
+
+main()
