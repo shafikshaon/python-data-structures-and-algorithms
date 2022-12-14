62ae773101001fa80fb8521cd3ecb9a1241d0f96
shafikshaon <shafikshaon@gmail.com>
2022-12-15 01:40:52 +0600

    Largest Number Among Three Numbers Add

---
+++ basics/maths/largest_number_among_three_numbers.py
@@ -0,0 +1,17 @@
+# Find the largest number among three number
+
+def main():
+    a = 10
+    b = 14
+    c = 12
+
+    if a >= b and a >= c:
+        largest = a
+    elif b >= a and b >= c:
+        largest = b
+    else:
+        largest = c
+    print(f"Largest number is: {largest}")
+
+
+main()
