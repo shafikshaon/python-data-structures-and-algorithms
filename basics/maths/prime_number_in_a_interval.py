a039cd68ecae3ec68530ba2316919f714a686770
shafikshaon <shafikshaon@gmail.com>
2022-12-15 00:53:20 +0600

    Prime Number in a Interval Add

---
+++ basics/maths/prime_number_in_a_interval.py
@@ -0,0 +1,17 @@
+# Print prime number in an interval
+
+def main():
+    lower_number = 900
+    upper_number = 1000
+    for num in range(lower_number, upper_number + 1):
+        flag = False
+        if lower_number > 1:
+            for i in range(2, num):
+                if num % i == 0:
+                    flag = True
+                    break
+        if not flag:
+            print(f"{num} is a prime number")
+
+
+main()
