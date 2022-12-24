de5b9d6bc5a4b7ae94367134dd89535d858fb944
shafikshaon <shafikshaon@gmail.com>
2022-12-24 23:53:46 +0600

    Reformat Find Maximum in Sliding Window

--- educative_grokking_coding_interview_patterns/03_sliding_window/find_maximum_in_sliding_window.py
+++ educative_grokking_coding_interview_patterns/03_sliding_window/find_maximum_in_sliding_window.py
@@ -25,14 +25,18 @@ def find_max_sliding_window(nums, window_size):
         # For every element, remove the previous smaller elements from window
         while window and nums[i] >= nums[window[-1]]:
             check = True
-            print(f"\t\tnums[{i}] = {nums[i]} is greater than or equal to nums[window[-1]] = {nums[window[-1]]}")
+            print(
+                f"\t\tnums[{i}] = {nums[i]} is greater than or equal to nums[window[-1]] = {nums[window[-1]]}"
+            )
             window.pop()
             print("\t\tWindow after popping:", window)
 
         # this is only for the purpose of printing
         if not check:
             if window:
-                print(f"\t\tnums[{i}] = {nums[i]} is less than nums[window[-1]] = {nums[window[-1]]}")
+                print(
+                    f"\t\tnums[{i}] = {nums[i]} is less than nums[window[-1]] = {nums[window[-1]]}"
+                )
             else:
                 print(f"\t\tThe window is empty.")
         check = False
@@ -50,14 +54,18 @@ def find_max_sliding_window(nums, window_size):
         # from the tail of list
         while window and nums[i] >= nums[window[-1]]:
             check = True
-            print(f"\t\tnums[{i}] = {nums[i]} is greater than or equal to nums[window[-1]] = {nums[window[-1]]}")
+            print(
+                f"\t\tnums[{i}] = {nums[i]} is greater than or equal to nums[window[-1]] = {nums[window[-1]]}"
+            )
             window.pop()
             print("\t\tWindow after popping:", window)
 
         # this is only for the purpose of printing
         if not check:
             if window:
-                print(f"\t\tnums[{i}] = {nums[i]} is less than nums[window[-1]] = {nums[window[-1]]}")
+                print(
+                    f"\t\tnums[{i}] = {nums[i]} is less than nums[window[-1]] = {nums[window[-1]]}"
+                )
             else:
                 print(f"\t\tThe window is empty.")
         check = False
@@ -77,24 +85,53 @@ def find_max_sliding_window(nums, window_size):
 
 def main():
     target_list = [3, 3, 3, 3, 2, 4, 3, 2, 3, 18]
-    nums_list = [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-                 [10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
-                 [10, 10, 10, 10, 10, 10, 10, 10, 10, 10],
-                 [1, 5, 8, 10, 10, 10, 12, 14, 15, 19, 19, 19, 17, 14, 13, 12, 12, 12, 14, 18, 22, 26, 26, 26, 28, 29, 30],
-                 [10, 6, 9, -3, 23, -1, 34, 56, 67, -1, -4, -8, -2, 9, 10, 34, 67],
-                 [4, 5, 6, 1, 2, 3],
-                 [9, 5, 3, 1, 6, 3],
-                 [2, 4, 6, 8, 10, 12, 14, 16],
-                 [-1, -1, -2, -4, -6, -7],
-                 [4, 4, 4, 4, 4, 4]]
+    nums_list = [
+        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        [10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
+        [10, 10, 10, 10, 10, 10, 10, 10, 10, 10],
+        [
+            1,
+            5,
+            8,
+            10,
+            10,
+            10,
+            12,
+            14,
+            15,
+            19,
+            19,
+            19,
+            17,
+            14,
+            13,
+            12,
+            12,
+            12,
+            14,
+            18,
+            22,
+            26,
+            26,
+            26,
+            28,
+            29,
+            30,
+        ],
+        [10, 6, 9, -3, 23, -1, 34, 56, 67, -1, -4, -8, -2, 9, 10, 34, 67],
+        [4, 5, 6, 1, 2, 3],
+        [9, 5, 3, 1, 6, 3],
+        [2, 4, 6, 8, 10, 12, 14, 16],
+        [-1, -1, -2, -4, -6, -7],
+        [4, 4, 4, 4, 4, 4],
+    ]
 
     for i in range(len(nums_list)):
         print(i + 1, ". Original array:\t", nums_list[i], sep="")
         print("Window size:\t\t", target_list[i])
-        print("\n Max:\t\t",
-              find_max_sliding_window(nums_list[i], target_list[i]))
-        print("-"*100)
+        print("\n Max:\t\t", find_max_sliding_window(nums_list[i], target_list[i]))
+        print("-" * 100)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
