331cb5b69253ef4bf63b4835b561d48c5c59b1f9
shafikshaon <shafikshaon@gmail.com>
2023-01-02 22:48:54 +0600

    Reverse Integer Solution Add

---
+++ leetcode/top_interview_questions/easy/string/02_reverse_integer.py
@@ -0,0 +1,31 @@
+def main(x: int) -> int:
+    print(f"Original value: {x}.\n")
+    result = 0
+    sign = 1
+
+    if x < 0:
+        sign = -1
+        x = x * sign
+
+    while x > 0:
+        print(f"x is now: {x}.")
+        remainder = x % 10
+        x //= 10
+        print(f"Remainder: {remainder}. Quotient: {x}.")
+        result = result * 10 + remainder
+        print(f"Current reversed value: {result}. and x is: {x}.\n")
+
+    min_val = 2**31 * -1
+    max_val = 2**31 - 1
+    if result > max_val or result < min_val:
+        return 0
+    print(f"\nAfter reversing the x is: {result*sign}.")
+    return result * sign
+
+
+main(x=-123)
+
+# Output
+
+# Time complexity - O(n)
+# Space complexity - O(1)
