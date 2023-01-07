989946cf3dc943df56f924475b30c534aee1694d
shafikshaon <shafikshaon@gmail.com>
2023-01-07 18:06:43 +0600

    Contains Duplicate Solution

---
+++ leetcode_top_interview_questions_easy/array/04_contains_duplicate.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+def main(nums: List[int]) -> bool:
+    hash_table = {}
+    for i in nums:
+        if i in hash_table:
+            print(f"Found {i} already found in array.")
+            print(f"Value appears at least twice: Yes")
+            return True
+        else:
+            print(f"Value {i} push to hash table.")
+            hash_table[i] = 1
+    print(f"Value appears at least twice: No")
+    return False
+
+
+main(nums=[1, 2, 3, 4, 4])
+
+
+# Time complexity - O(n)
+# Space complexity - O(n)
