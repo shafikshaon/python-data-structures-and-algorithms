e7e64a45d52d969c0209a8e8a38394514e6f7960
shafikshaon <shafikshaon@gmail.com>
2023-01-11 22:14:28 +0600

    Valid Anagram Solution Add

---
+++ leetcode/top_interview_questions/easy/string/04_valid_anagram.py
@@ -0,0 +1,42 @@
+def main(s: str, t: str) -> bool:
+    print(f"Original value of s: {s}.")
+    print(f"Original value of t: {t}.\n")
+
+    if len(s) != len(t):
+        print(f"The input string s ({s}) and t ({t}) length are not same.")
+        return False
+
+    d = {}
+    print(f"Creating hash table for first input string: {s}.")
+    for c in s:
+        if c in d:
+            d[c] += 1
+        else:
+            d[c] = 1
+    print(f"After creating hash table the dictionary is: {d}.\n")
+    print(
+        "Loop through the second string and decrement the count of each letter in the dictionary\n"
+    )
+
+    for c in t:
+        if c in d:
+            print(f"Found {c} in d. So decrementing occurrence from hash table.")
+            d[c] -= 1
+            if d[c] < 0:
+                print(
+                    f"Character {c} appear more than first string: {s}. So, it's not valid anagram."
+                )
+                return False
+        else:
+            print(f"Character {c} not found in hash table. So, it's not valid anagram.")
+            return False
+    print(f"\nNow hash table is: {d}.")
+    print("All the counts are 0, then t is an anagram of s")
+    print(f'\n"{s}" is valid anagram.')
+    return True
+
+
+main(s="anagram", t="nagaram")
+
+# Time complexity - O(n)
+# Space complexity - O(1)
