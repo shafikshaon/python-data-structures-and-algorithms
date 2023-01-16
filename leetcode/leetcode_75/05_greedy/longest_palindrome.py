7588fe7f5d4b0db1f2d9fa87b0b440f3b219e1ca
shafikshaon <shafikshaon@gmail.com>
2023-01-17 02:46:31 +0600

    Add Output for Longest Palindrome

--- leetcode/leetcode_75/05_greedy/longest_palindrome.py
+++ leetcode/leetcode_75/05_greedy/longest_palindrome.py
@@ -29,5 +29,38 @@ main(s="a")
 
 # Output
 """
+Input string is: abccccdd.
 
+"a" add to: set().
+Character set now: {'a'}.
+
+"b" add to: {'a'}.
+Character set now: {'b', 'a'}.
+
+"c" add to: {'b', 'a'}.
+Character set now: {'b', 'a', 'c'}.
+
+"c" remove from: {'b', 'a', 'c'}.
+Character set now: {'b', 'a'}.
+
+"c" add to: {'b', 'a'}.
+Character set now: {'b', 'a', 'c'}.
+
+"c" remove from: {'b', 'a', 'c'}.
+Character set now: {'b', 'a'}.
+
+"d" add to: {'b', 'a'}.
+Character set now: {'b', 'a', 'd'}.
+
+"d" remove from: {'b', 'a', 'd'}.
+Character set now: {'b', 'a'}.
+
+Longest length of palindrome: 7.
+-----------------------------------------------------------------------------------------------------
+Input string is: a.
+
+"a" add to: set().
+Character set now: {'a'}.
+
+Longest length of palindrome: 1.
 """
