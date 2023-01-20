d5424e86890d5e1bac9af6206db4f8894b926699
Shafikur Rahman <shafikshaon@gmail.com>
2023-01-21 00:22:16 +0600

    Update Validate Binary Search Tree for Displaying Tree

--- leetcode/leetcode_75/08_binary_search_tree/validate_binary_search_tree.py
+++ leetcode/leetcode_75/08_binary_search_tree/validate_binary_search_tree.py
@@ -21,12 +21,45 @@ class Node:
             self.data = data
 
     # Print the tree
-    def print_tree(self):
-        if self.left:
-            self.left.print_tree()
-        print(self.data),
-        if self.right:
-            self.right.print_tree()
+    def print_tree(root):
+        def height(root):
+            return 1 + max(height(root.left), height(root.right)) if root else -1
+
+        nlevels = height(root)
+        width = pow(2, nlevels + 1)
+
+        q = [(root, 0, width, 'c')]
+        levels = []
+
+        while (q):
+            node, level, x, align = q.pop(0)
+            if node:
+                if len(levels) <= level:
+                    levels.append([])
+
+                levels[level].append([node, level, x, align])
+                seg = width // (pow(2, level + 1))
+                q.append((node.left, level + 1, x - seg, 'l'))
+                q.append((node.right, level + 1, x + seg, 'r'))
+
+        for i, l in enumerate(levels):
+            pre = 0
+            preline = 0
+            linestr = ''
+            pstr = ''
+            seg = width // (pow(2, i + 1))
+            for n in l:
+                valstr = str(n[0].data)
+                if n[3] == 'r':
+                    linestr += ' ' * (n[2] - preline - 1 - seg - seg // 2) + '¯' * (seg + seg // 2) + '\\'
+                    preline = n[2]
+                if n[3] == 'l':
+                    linestr += ' ' * (n[2] - preline - 1) + '/' + '¯' * (seg + seg // 2)
+                    preline = n[2] + seg + seg // 2
+                pstr += ' ' * (n[2] - pre - len(valstr)) + valstr  # correct the potition acording to the number size
+                pre = n[2]
+            print(linestr)
+            print(pstr)
 
     def is_valid_bst(self):
         root = self
