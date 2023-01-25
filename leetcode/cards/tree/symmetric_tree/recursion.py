52b247456d520dd488ef8d224d1fed87e125025d
Shafikur Rahman <shafikshaon@gmail.com>
2023-01-25 20:50:15 +0600

    Reformat Symmetric Tree Code

--- leetcode/cards/tree/symmetric_tree/recursion.py
+++ leetcode/cards/tree/symmetric_tree/recursion.py
@@ -1,7 +1,7 @@
 class TreeNode:
     answer = []
 
-    def __init__(self, val="", left=None, right=None):
+    def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
@@ -80,13 +80,16 @@ class TreeNode:
         return is_symmetric(root, root)
 
 
-_root = TreeNode("1")
-_root.left = TreeNode("2")
-_root.right = TreeNode("2")
-_root.left.left = TreeNode("3")
-_root.left.right = TreeNode("4")
-_root.right.left = TreeNode("4")
-_root.right.right = TreeNode("3")
+_root = TreeNode(5)
+_root.left = TreeNode(4)
+_root.right = TreeNode(8)
+_root.left.left = TreeNode(11)
+_root.left.left.left = TreeNode(7)
+_root.left.left.right = TreeNode(2)
+
+_root.right.left = TreeNode(13)
+_root.right.right = TreeNode(4)
+_root.right.right.right = TreeNode(1)
 
 _root.print_tree()
 
