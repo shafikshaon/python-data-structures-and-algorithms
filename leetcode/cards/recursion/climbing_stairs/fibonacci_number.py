a5ed757746e779d3c172ab1f16fe32485f76b914
Shafikur Rahman <shafikshaon@gmail.com>
2023-01-31 00:56:21 +0600

    Climbing Stairs Fibonacci Number Approach Solution Update

--- leetcode/cards/recursion/climbing_stairs/fibonacci_number.py
+++ leetcode/cards/recursion/climbing_stairs/fibonacci_number.py
@@ -1,20 +1,20 @@
 def main(n):
     if n == 1:
-        return n
+        return 1
     prev1 = 1
     prev2 = 2
     for i in range(3, n + 1):
         current = prev1 + prev2
-        prev1 = prev1
+        prev1 = prev2
         prev2 = current
     return prev2
 
 
-print(f"Distinct ways: {main(n=3)}.")
+print(f"Distinct ways: {main(n=5)}.")
 
 """
 Output:
-Distinct ways: 3.
+Distinct ways: 8.
 
 Time complexity: O(n)
 Space complexity: O(1)
