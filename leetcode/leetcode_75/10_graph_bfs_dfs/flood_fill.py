084bc4c113ce8c03f6701c376dd983dd2bdfbd3c
Shafikur Rahman <shafikshaon@gmail.com>
2023-02-03 20:47:48 +0600

    Flood Fill Output and Complexity Add

--- leetcode/leetcode_75/10_graph_bfs_dfs/flood_fill.py
+++ leetcode/leetcode_75/10_graph_bfs_dfs/flood_fill.py
@@ -2,11 +2,22 @@ from typing import List
 
 
 class Solution:
-    def print_row(self,row):
-        return ' ' + '___ '*len(row) + ' \n' + '|' + '|'.join(f' {x} ' for x in row) + '|'
+    def print_row(self, row):
+        return (
+            " "
+            + "___ " * len(row)
+            + " \n"
+            + "|"
+            + "|".join(f" {x} " for x in row)
+            + "|"
+        )
 
-    def display_board(self,image: List[List[int]]):
-        return '\n'.join(self.print_row(row) for row in image) + '\n ' + '--- '*len(image[-1])
+    def display_board(self, image: List[List[int]]):
+        return (
+            "\n".join(self.print_row(row) for row in image)
+            + "\n "
+            + "--- " * len(image[-1])
+        )
 
     def flood_fill(self, image, sr, sc, new_color):
         row, col = len(image), len(image[0])
@@ -33,6 +44,32 @@ class Solution:
 sol = Solution()
 print(f"Input image:")
 print(sol.display_board(image=[[1, 1, 1], [1, 1, 0], [1, 0, 1]]))
-output = sol.flood_fill(image=[[1, 1, 1], [1, 1, 0], [1, 0, 1]], sr=1, sc=1, new_color=2)
+output = sol.flood_fill(
+    image=[[1, 1, 1], [1, 1, 0], [1, 0, 1]], sr=1, sc=1, new_color=2
+)
 print(f"Output image:")
 print(sol.display_board(output))
+
+
+"""
+Output:
+Input image:
+ ___ ___ ___  
+| 1 | 1 | 1 |
+ ___ ___ ___  
+| 1 | 1 | 0 |
+ ___ ___ ___  
+| 1 | 0 | 1 |
+ --- --- --- 
+Output image:
+ ___ ___ ___  
+| 2 | 2 | 2 |
+ ___ ___ ___  
+| 2 | 2 | 0 |
+ ___ ___ ___  
+| 2 | 0 | 1 |
+ --- --- --- 
+
+Time Complexity: O(n)
+Space Complexity: O(n)
+"""
