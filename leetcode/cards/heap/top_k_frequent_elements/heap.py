74c5a24182babf1e2b14c9bac6061456416a5d33
Shafikur Rahman <shafikshaon@gmail.com>
2023-02-09 01:08:25 +0600

    Top K Frequent Elements Solution Add

---
+++ leetcode/cards/heap/top_k_frequent_elements/heap.py
@@ -0,0 +1,25 @@
+import heapq
+from collections import Counter
+from typing import List
+
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        if k == len(nums):
+            return nums
+
+        count = Counter(nums)
+        return heapq.nlargest(k, count.keys(), key=count.get)
+
+
+sol = Solution()
+print(sol.topKFrequent(nums=[1, 1, 1, 2, 2, 3], k=2))
+
+
+"""
+Output:
+[1, 2]
+
+Time complexity: O(nlogk)
+Space complexity: O(n+k)
+"""
